bdf9cc2c07220625164fccf5fbfdd222ea20646d
selvaragavan C <selvaragavan.ct@gmail.com>
2025-01-25 22:53:59 +0530

    Initial commit

--- src/main/java/com/selvaragavan/afterthecontestapi/authentication/repositories/RegisterUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.selvaragavan.afterthecontestapi.authentication.repositories;
-
-import com.selvaragavan.afterthecontestapi.authentication.Entities.RegisteredUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterUserRepository extends JpaRepository<RegisteredUser,String> {
-    public RegisteredUser findByEmail(String email);
-}
